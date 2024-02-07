7454d1f2418c473781833ad063aeadfbe97bd0ba
Akryllax <alexmontboik@gmail.com>
2024-02-07 15:57:40 +0100

    asdf

--- src/game/TestShip.cpp
+++ src/game/TestShip.cpp
@@ -3,45 +3,46 @@
 namespace Akr {
 
 TestShip::TestShip(std::string const& name) : RigidBody(name, &(Core::GetDataLayer<PhysicsLayer>()->GetBox2DWorld())) {
-    starshipDef.type = b2_dynamicBody;
-    setBitmapResource(std::make_shared<BitmapResource>("1.png"));
-    GetBitmapResource()->load();
-    assert(body);
+  starshipDef.type = b2_dynamicBody;
+  setBitmapResource(std::make_shared<BitmapResource>("1.png"));
+  GetBitmapResource()->load();
+  assert(body);
 }
 
 void TestShip::render() {
-    // Deprecated render function implementation
+  // Deprecated render function implementation
 }
 
 std::shared_ptr<Renderer::RenderCommand> TestShip::GenerateRenderCommand() {
-    auto compositeCommand = std::make_shared<Akr::Renderer::CompositeRenderComand>();
+  auto compositeCommand = std::make_shared<Akr::Renderer::CompositeRenderComand>();
 
-    Math::Rect rect{this->GetBody()->GetPosition().x, this->GetBody()->GetPosition().y,
-                    this->GetBody()->GetPosition().x, this->GetBody()->GetPosition().y, this->GetBody()->GetAngle()};
+  Math::Rect rect{this->GetBody()->GetPosition().x, this->GetBody()->GetPosition().y,
+                  this->GetBody()->GetPosition().x, this->GetBody()->GetPosition().y, this->GetBody()->GetAngle()};
 
-    compositeCommand->QueueCommand(
-        std::make_shared<Renderer::DrawBitmapOperation>(GetBitmapResource()->getBitmap(), rect));
+  compositeCommand->QueueCommand(
+      std::make_shared<Renderer::DrawBitmapOperation>(GetBitmapResource()->getBitmap(), rect));
 
-    Akr::Core::GetDataLayer<Akr::RendererLayer>()->GetDebugRenderer().DrawArrow(
-        this->GetBody()->GetPosition(), this->GetBody()->GetPosition() + this->GetBody()->GetLinearVelocity());
+  Akr::Core::GetDataLayer<Akr::RendererLayer>()->GetDebugRenderer().DrawArrow(
+      this->GetBody()->GetPosition(), this->GetBody()->GetPosition() + this->GetBody()->GetLinearVelocity());
 
-    return compositeCommand;
+  return compositeCommand;
 }
 
 void TestShip::TakeControl() {
-    // Implementation of TakeControl method
+  // Implementation of TakeControl method
 }
 
 void TestShip::ReleaseControl() {
-    // Implementation of ReleaseControl method
+  // Implementation of ReleaseControl method
 }
 
 void TestShip::OnEvent() {
-    // Implementation of OnEvent method
+  // Implementation of OnEvent method
 }
 
 void TestShip::OnRawInput(std::chrono::milliseconds const& delta) {
-    // Implementation of OnRawInput method
+  // Implementation of OnRawInput method
+
 }
 
 } // namespace Akr
